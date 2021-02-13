371a982fd3aca5f96b1fd56cc443eee1a0215402
Kosm8 <77066077+Kosm8@users.noreply.github.com>
2021-02-13 03:49:02 +0100

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -3,4 +3,5 @@ sudo apt-get upgrade
 sudo apt-get install ragel cvs yasm pax nasm gperf autogen autoconf-archive python3-setuptools
 sudo apt-get update --fix-missing
 sudo apt-get install -f
+sudo apt-get --fix-broken install
 sudo -H pip3 install meson ninja
